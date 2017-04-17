4bb37db6861a516d8b53ae9b29b9a698dd4eca58
Garett Roberts <mygarett@gmail.com>
2017-04-16 18:03:31 -0700

    Basic testing, needs more testing

---
+++ test/tests/algorithmtest.py
@@ -0,0 +1,75 @@
+'''
+@author: Garett Roberts
+
+Algorithm testing
+
+'''
+import os
+import argparse
+import sys
+#for bypassing import paths
+curpth    = os.path.dirname(os.path.abspath(__file__))
+targetpth = curpth + '/../../src/'
+sys.path.insert(0, targetpth)
+
+from team import Team
+from day import Day
+from student import Student
+from algorithm import AlgorithmManager
+
+if __name__ == "__main__":
+    algorithm  = AlgorithmManager()
+    studentlis = []
+
+    d1 = Day("Tuesday")
+    d1.insertTime(10)
+    d1.insertTime(13)    
+    d2 = Day("Wednesday")
+    d2.insertTime(10)
+    d2.insertTime(14)
+
+    s1 = Student("Hope, Bob","mahananaka@gmail.com")
+    days = [d1, d2]
+    filters1 = {}
+    filters1['Meeting Times'] = days
+    s1.setFilters(filters1)
+    studentlis.append(s1)
+
+    s2 = Student("Jensen, Emily","")
+    days = [d2]
+    filters2 = {}
+    filters2['Meeting Times'] = days
+    s2.setFilters(filters2)
+    studentlis.append(s2)
+
+    d1 = Day("Tuesday")
+    d1.insertTime(10)
+    d1.insertTime(13)    
+    d2 = Day("Wednesday")
+    d2.insertTime(10)
+    d2.insertTime(14)
+
+    s3 = Student("Streisand, Barbara","")
+    days = [d1,d2]
+    filters3 = {}
+    filters3['Meeting Times'] = days
+    s3.setFilters(filters3)
+    studentlis.append(s3)
+
+    s4 = Student("Cobain, Curt","")
+    days = [d2]
+    filters4 = {}
+    filters4['Meeting Times'] = days
+    s4.setFilters(filters4)
+    studentlis.append(s4)
+
+    s5 = Student("Dog, Man", "")
+    days = [d2]
+    filters5 = {}
+    filters5['Meeting Times'] = days
+    s5.setFilters(filters5)
+    studentlis.append(s5)
+        
+    teamset = algorithm.initTeamSet(studentlis)
+
+    [print(i.getRating()) for i in teamset] 
