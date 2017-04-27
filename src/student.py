6fc86c298ea770cc2aeda8a299cc57c68d0fd880
Jared Paeschke <paeschke@cs.uoregon.edu>
2017-04-27 12:13:59 -0700

    removed commented out code

--- src/student.py
+++ src/student.py
@@ -44,13 +44,6 @@ class Student:
 	def getPrefs(self):
 		return self.__filters
 
-	'''
-	def getDays(self):
-		return self.__days
-
-	def getLangPrefs(self):
-		return self.__langprefs
-	'''
 	#end getters
 
 	#start setters
@@ -75,102 +68,6 @@ class Student:
 			self.__filters = f
 			return True
 	#end setters
-
-	'''
-	#associates a day with a student, won't allow days with the same name
-	#prevents things that are not a 'Day' type from being added. Returns
-	#true or false if insert is successful
-	def insertDay(self, day):
-		if type(day).__name__ != "Day":
-			return False
-
-		for i in range(len(self.__days)):
-			if day == self.__days[i]:
-				return False
-
-		self.__days.append(day)
-		return True
-
-	#removes day from the list of days associated with this student if
-	#day equals a day in the list
-	def remDay(self, day):
-		if type(day).__name__ != "Day":
-			return False
-
-		for i in range(len(self.__days)):
-			if day == self.__days[i]:
-				del self.__days[i]
-				return True
-
-		return False
-
-	#purges the list of days associated with this student, this will also 
-	#clear anything that references self.__days as well (i.e. cpy = self.__days
-	#cpy will be cleared as well)
-	def purgeDays(self):
-		del self.__days[:]
-
-        #Insert a prefered teammate to the prefered teammates list. 
-        #The teammate will only be added if they are not already in the
-        #list. If the add is a succes, True is returned. Otherwise, False. 
-	def insertTeammatePref(self,other):
-		if not isinstance(other, str):
-			return False
-
-		if other in self.__teammateprefs:
-			return True
-
-		self.__teammateprefs.append(other)
-		return True
-        
-        #Remove a teammate preference. Returns True if succesful, 
-        #false otherwise. 
-	def remTeammatePref(self,other):
-		if not isinstance(other, str):
-			return False
-
-		if other in self.__teammateprefs:
-			self.__teammateprefs.remove(other)
-			return True            
-
-		return False
-
-        #Reset the teammate preferences to an empty list
-	def purgeTeammatePrefs(self):
-		self.__teammateprefs = []
-
-	#associates a language with a students preferences, must pass a string
-	#if the string is already in the list it will not add it. Returns true
-	#or false if insert is successful
-	def insertLangPref(self,lang):
-		if not isinstance(lang,str):
-			return False
-
-		for i in range(len(self.__langprefs)):
-			if lang == self.__langprefs[i]:
-				return False
-
-		self.__langprefs.append(lang)
-		return True
-
-	#removes a langue preference from the student. Only accepts
-	#a string and returns true removal occurs, otherwise false
-	def remLangPref(self, lang):
-		if not isinstance(lang,str):
-			return False
-
-		for i in range(len(self.__langprefs)):
-			if lang == self.__langprefs[i]:
-				del self.__langprefs[i]
-				return True
-		return False
-
-	#purges the list of language preferences associated with this student, 
-	#this will also clear anything that references self.__langprefs as well.
-	#(i.e. cpy = self.__langprefs; cpy will be cleared as well)
-	def purgeLangPrefs(self):
-		del self.__langprefs[:]
-	'''
 
 	#compares two instances of Student based on their email address
 	#this returns true when both have the same email
